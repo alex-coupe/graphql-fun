82ebf710888dc71e4b947434a7bf4e97fde51445
Alex Coupe <acoupe@gmail.com>
2020-01-19 15:21:28 +0000

    First query with Graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+
+const films = [
+    {name: 'A New Hope', genre: 'Sci-Fi', id:'1' },
+    {name: 'The Two Towers', genre: 'Fantasy', id: 2},
+    {name: 'Avengers', genre: 'Superhero', id:'3'}
+];
 
 const FilmType = new GraphQLObjectType({
     name: 'Film',
@@ -9,4 +17,22 @@ const FilmType = new GraphQLObjectType({
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        film: {
+            type: FilmType,
+            args: {id: {type:GraphQLString}},
+            resolve(parent,args){
+                // get data from DB / other source
+                return _.find(films, {id:args.id});
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
