b85cd36b7b97c6e9f68dd38a968416ba91bc73aa
Alex Coupe <acoupe@gmail.com>
2020-01-19 15:36:09 +0000

    Added director type with some fake data to query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,37 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
 
-
+/////DUMMY DATA
 const films = [
     {name: 'A New Hope', genre: 'Sci-Fi', id:'1' },
     {name: 'The Two Towers', genre: 'Fantasy', id: '2'},
     {name: 'Avengers', genre: 'Superhero', id:'3'}
 ];
 
+const directors = [
+    {name: 'George Lucas', age: 75, id:'1' },
+    {name: 'Peter Jackson', age: 58, id: '2'},
+    {name: 'Joss Whedon', age: 55, id:'3'}
+];
+////////
+
 const FilmType = new GraphQLObjectType({
     name: 'Film',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
+    })
+});
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
 });
 
@@ -24,10 +40,17 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         film: {
             type: FilmType,
-            args: {id: {type:GraphQLString}},
+            args: {id: {type:GraphQLID}},
             resolve(parent,args){
                 // get data from DB / other source
                 return _.find(films, {id:args.id});
+            }
+        },
+        director: {
+            type: DirectorType,
+            args: {id: {type:GraphQLID}},
+            resolve(parent, args){
+                return _.find(directors, {id:args.id});
             }
         }
     }
