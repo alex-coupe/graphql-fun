deca361eb6ec93ca35e7924d142573e1080216bb
Alex <alexcoupe@xperisoft.com>
2020-01-20 13:58:08 +0000

    Connecting to a mongo instance

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
 
 const app = express();
+
+mongoose.connect(process.env.ConnectionString,{ useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
