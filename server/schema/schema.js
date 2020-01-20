cfa9a7f8ff0f7017b113951429fb417d4c478050
Alex <alexcoupe@xperisoft.com>
2020-01-20 12:11:36 +0000

    Relationships between data and getting lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,16 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
 /////DUMMY DATA
 const films = [
-    {name: 'A New Hope', genre: 'Sci-Fi', id:'1' },
-    {name: 'The Two Towers', genre: 'Fantasy', id: '2'},
-    {name: 'Avengers', genre: 'Superhero', id:'3'}
+    {name: 'A New Hope', genre: 'Sci-Fi', id:'1', directorId:'1' },
+    {name: 'The Two Towers', genre: 'Fantasy', id: '2', directorId: '2'},
+    {name: 'Avengers Assemble', genre: 'Superhero', id:'3', directorId: '3'},
+    {name: 'The Hobbit', genre: 'Fantasy', id: '4', directorId: '2'},
+    {name: 'Return of the King', genre: 'Fantasy', id: '5', directorId: '2'},
+    {name: 'Avengers: Age of Ultron', genre: 'Superhero', id:'6', directorId: '3'},
 ];
 
 const directors = [
@@ -22,7 +25,13 @@ const FilmType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        director: {
+            type: DirectorType,
+            resolve(parent, args){
+                return _.find(directors, {id:parent.directorId});
+            }
+        }
     })
 });
 
@@ -31,7 +40,13 @@ const DirectorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        films: {
+            type: new GraphQLList(FilmType),
+            resolve(parent, args){
+                return _.filter(films, {directorId: parent.id});
+            }
+        }
     })
 });
 
